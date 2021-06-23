5c1bf2b735fe90aa4bca12ce44064f1e26cd3a55
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-23 19:05:45 -0300

    easy uninstall added

---
+++ push/install.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("sudo mv ./push. /bin && sudo mv ./push.. /bin");
+
+}
---
+++ push/uninstall.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("sudo mv /bin/push. ./ && sudo mv /bin/push.. ./");
+
+}
