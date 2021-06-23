2b2e4e881eff9d24d66f85f6c740b0ef8269c9b4
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-23 17:29:08 -0300

    Changing

--- push/main.c
+++ push/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
-    printf("Executing git add .\n ");
     system("git add .");
     char commit[1024] = "git commit -m ";
     char summary[51];
@@ -11,9 +10,8 @@ int main(){
     strcat(commit, "'");
     strcat(summary, "'");
     strcat(commit, summary);
-    printf("Executing git commit -m <summary>...\n");
     system(commit);
-    printf("Executing git push...\n");
     system("git push");
+    printf("\nSucceful!!!");
     return 0;
 }
