77b218746927512aea3f21ebd139e7ecf2cea820
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-23 18:23:54 -0300

    testando testando testando testando testando testa

--- push/main.c
+++ push/main.c
@@ -3,8 +3,8 @@
 #include <string.h>
 int main(){
     system("git add .");
-    char commit[51] = "git commit -m ";
-    char summary[1024];
+    char commit[1024] = "git commit -m ";
+    char summary[51];
     printf("Commit summary: ");
     fgets(summary, sizeof(summary), stdin);
     strcat(commit, "'");
